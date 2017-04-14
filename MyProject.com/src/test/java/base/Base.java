2af186471cd486f587643eabb132ef90f46bf38b
ankit.a.jai <ankit.a.jai@BDC8-L-60219P8.dir.svc.accenture.com>
2017-04-14 13:55:14 +0530

    send base

--- MyProject.com/src/test/java/base/Base.java
+++ MyProject.com/src/test/java/base/Base.java
@@ -1,18 +1,18 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class Base {
-	WebDriver driver;
+	 public WebDriver driver;
 	@BeforeClass
 	public void Precondition()
 	{
-		System.setProperty("", "C:/Users/ankit.a.jai/Downloads/geckodriver-v0.15.0-win64/geckodriver.exe");
-		driver= new RemoteWebDriver(DesiredCapabilities.firefox());
+		System.setProperty("webdriver.gecko.driver", "C:/Users/ankit.a.jai/Downloads/geckodriver-v0.15.0-win64/geckodriver.exe");
+		//driver= new RemoteWebDriver(DesiredCapabilities.firefox());
+		driver= new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.co.in");
 	}
